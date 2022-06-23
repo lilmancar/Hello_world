e8e428bda994b408e70246249527157ba4e55651
Lev <lilmancar2@rambler.ru>
2022-06-23 19:19:52 +0300

    добавил папку Example006

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 12;
+int b = 7;
+int c = 4;
+int d = 10;
+int e = 11;
+int max = a;
+if (a > max ) max = a;
+if (b > max ) max = b;
+if (c > max ) max = c;
+if (d > max ) max = d;
+if (e > max ) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
