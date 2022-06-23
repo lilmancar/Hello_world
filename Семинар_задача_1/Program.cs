9021f10332ba272070dcfa1c658b8317500cc0ed
Lev <lilmancar2@rambler.ru>
2022-06-23 18:40:10 +0300

    добавил папку Семинар_задача_1

---
+++ Семинар_задача_1/Program.cs
@@ -0,0 +1,5 @@
+﻿int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberB * numberB == numberA) Console.Write ("yes");
+else Console.Write ("no");
+
