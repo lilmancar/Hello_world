f8cb866516c83f86f6bf93e51b2cf1a123de2559
Lev <lilmancar2@rambler.ru>
2022-07-04 22:47:22 +0300

    выполнил дз4

---
+++ Homework_04/Задача_25/Program.cs
@@ -0,0 +1,9 @@
+﻿double a, b, c; 
+Console.WriteLine("Приветствую! ");
+Console.WriteLine("Это программа возведения числа в степень");
+Console.WriteLine("Введите число, которое вы хотите возвести в степень: ");
+a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите степень, в которую хотите возвести число "+a+":");
+b = Convert.ToDouble(Console.ReadLine());
+c = Math.Pow(a, b);
+Console.WriteLine("Готово! " +a+ " в степени "+b+" равно: " +c+ "!");
