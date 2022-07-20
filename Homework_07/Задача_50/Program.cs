d6d8988355cb97117377dca2a4f104f8d9e25111
Lev <lilmancar2@rambler.ru>
2022-07-20 15:38:16 +0300

    выполнил дз 07

---
+++ Homework_07/Задача_50/Program.cs
@@ -0,0 +1,46 @@
+﻿int[, ] CreateArray(int[, ] array)
+{
+     for (int i =0; i < array.GetLength(0); i++)
+     {
+        for(int j=0; j< array.GetLength(1); j++)
+        {  Console.WriteLine("Введите элемент массива: ");
+           int x = Convert.ToInt32(Console.ReadLine());
+           array[i,j] = x;
+        
+        }
+        Console.WriteLine();
+     }
+    for (int i =0; i < array.GetLength(0); i++)
+     {
+        for(int j=0; j< array.GetLength(1); j++)
+        {
+           Console.Write(array[i,j] + "\t"); // \t (табуляция)
+        }
+        Console.WriteLine();
+     }
+     return array;
+}
+
+
+void CheckArray(int i, int j, int[, ] array)
+{
+    if (i < array.GetLength(0) && j < array.GetLength(1) && i >= 0 && j >= 0)
+        Console.WriteLine(array[i,j]);
+    else
+        Console.WriteLine("Данного элемента нет в массиве!");
+}
+Console.WriteLine("Приветствую! ");
+Console.WriteLine("Эта программа на вход принимает позиции элемента в двумерном массиве");
+Console.WriteLine("и возвращает значение этого элемента или же указание,");
+Console.WriteLine("что такого элемента нет. ");
+Console.WriteLine("Давайте зададим массив ");
+Console.WriteLine("например размер массива 4 на 4 ");
+int[, ] array = new int[4, 4];
+array = CreateArray(array);
+Console.WriteLine("Введите номер позиции элемента по горизонтали ");
+Console.WriteLine("нумерация начинается с 0 ");
+int i = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер позиции элемента по вертикали ");
+Console.WriteLine("нумерация начинается с 0 ");
+int j = Convert.ToInt32(Console.ReadLine());
+CheckArray(i,j, array );
---
+++ Homework_07/Задача_52/Program.cs
@@ -0,0 +1,43 @@
+﻿int[, ] CreateArray(int[, ] array)
+{
+     for (int i =0; i < array.GetLength(0); i++)
+     {
+        for(int j=0; j< array.GetLength(1); j++)
+        {  
+           int x = new Random().Next(1,100);
+           array[i,j] = x;
+        
+        }
+        Console.WriteLine();
+     }
+    for (int i =0; i < array.GetLength(0); i++)
+     {
+        for(int j=0; j< array.GetLength(1); j++)
+        {
+           Console.Write(array[i,j] + "\t"); // \t (табуляция)
+        }
+        Console.WriteLine();
+     }
+     return array;
+}
+
+void AvgArray(int[, ] array)
+{
+    Console.WriteLine();
+ for(int i =0; i < array.GetLength(0);i++)
+ {
+    double avg = 0;
+    for(int j =0; j < array.GetLength(1);j++)
+    {
+        avg = avg + array[j,i];
+    }
+    Console.WriteLine(i + 1 + " столбик: " + avg/array.GetLength(0));
+ }
+}
+Console.WriteLine("Приветствую! ");
+Console.WriteLine("Эта программа находит среднее арифметическое элементов в каждом столбце. ");
+Console.WriteLine("Давайте зададим массив из целых рандомных чисел. ");
+Console.WriteLine("например размер массива 4 на 4 ");
+int[, ] array = new int[4, 4];
+array = CreateArray(array);
+AvgArray(array);
