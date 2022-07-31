64f24e9f1bd6687d432db119b8f3c9e7448d2a70
Lev <lilmancar2@rambler.ru>
2022-07-31 18:02:14 +0300

    выполнил last homework

---
+++ Homework_09/Задача_66/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Приветствую! ");
+Console.WriteLine("Эта программа находит сумму натуральных элементов в промежутке от M до N ");
+int m = InputNumbers("Введите M: ");
+int n = InputNumbers("Введите N: ");
+int temp = m;
+
+if (m > n) 
+{
+  m = n; 
+  n = temp;
+}
+
+PrintSumm(m, n, temp=0);
+
+void PrintSumm(int m, int n, int summ)
+{
+  summ = summ + n;
+  if (n <= m)
+  {
+    Console.Write($"Сумма элементов= {summ} ");
+    return;
+  }
+  PrintSumm(m, n - 1, summ);
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
