c2c20b8123bf1eb62fc67fb665b7da80ec4faa51
Lev <lilmancar2@rambler.ru>
2022-08-01 12:25:19 +0300

    переделал задачу_64

---
+++ Homework_09/Задача_64_2/Program.cs
@@ -0,0 +1,14 @@
+﻿void f(int M, int N)
+{
+    if (M != (N -1 ))
+   {
+    f(M -1, N );
+    Console.Write(M + " ");
+   }   
+}
+Console.WriteLine("Приветствую! ");
+Console.WriteLine("Эта программа выводит все натуральные числа в промежутке от M до N.");
+Console.WriteLine("Допустим M = 5, а N = 1 ");
+int M = 5;
+int N = 1;
+f(M,N);
